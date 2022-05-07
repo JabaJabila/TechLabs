562fa3347a46bbe348d8390f0d9799c7a198f0f5
Andreev Artem <andreev_3731@mail.ru>
2022-05-07 21:03:39 +0300

    feat: location IEquatable

--- Lab4/GeneticAlgo/AlgorithmLogic/Map/Location.cs
+++ Lab4/GeneticAlgo/AlgorithmLogic/Map/Location.cs
@@ -3,7 +3,7 @@ using AlgorithmLogic.Tools.Exceptions;
 
 namespace AlgorithmLogic.Map;
 
-public class Location
+public class Location : IEquatable<Location>
 {
     private static uint _xLimit;
     private static uint _yLimit;
@@ -31,5 +31,24 @@ public class Location
     public Location MoveOn(int x, int y)
     {
         return new Location((uint)((x + X) % _xLimit), (uint)((y + Y) % _yLimit));
+    }
+
+    public bool Equals(Location? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return X == other.X && Y == other.Y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((Location) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
     }
 }
