9cbfcd0b1f5f37bff6e11b33cceaf61fa8a86951
Andreev Artem <andreev_3731@mail.ru>
2022-05-09 16:58:17 +0300

    feat: add json config reader

---
+++ Lab4/GeneticAlgo/AlgorithmLogic/Configuration/JsonConfigReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using AlgorithmLogic.Tools.Exceptions;
+
+namespace AlgorithmLogic.Configuration;
+
+public class JsonConfigReader
+{
+    public ConfigModel ReadFromJsonFile(string path)
+    {
+        using var fs = new FileStream(path, FileMode.Open);
+        var config = JsonSerializer.Deserialize<ConfigModel>(fs);
+        if (config is null) throw new GeneticAlgoException("Config file incorrect");
+        return config;
+    }
+}
--- Lab4/GeneticAlgo/AlgorithmLogic/Program.cs
+++ Lab4/GeneticAlgo/AlgorithmLogic/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using AlgorithmLogic.Configuration;
+
+// using var fs = new FileStream(@"D:\TechLabs\genalgo_cfg.json", FileMode.Create);
+// var options = new JsonSerializerOptions { WriteIndented = true };
+// var cfg = new ConfigModel(1000, 700, 30, 10, 3, 1, 40, 22, 100, 10, 100, 3, 5, 10);
+// JsonSerializer.Serialize(fs, cfg, options);
+
+var reader = new JsonConfigReader();
+var cfg = reader.ReadFromJsonFile(@"D:\TechLabs\genalgo_cfg.json");
