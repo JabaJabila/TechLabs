87ddb392b05d7378c4b69250a0583bb31026c659
Andreev Artem <andreev_3731@mail.ru>
2022-05-11 09:57:09 +0300

    feat: add perfTips playground

---
+++ Lab4/GeneticAlgo/PerfTipsPlayground/Program.cs
@@ -0,0 +1,13 @@
+﻿using AlgorithmLogic.Configuration;
+using AlgorithmLogic.Evolution;
+using AlgorithmLogic.Tools.Loggers;
+
+
+var reader = new JsonConfigReader();
+var cfg = reader.ReadFromJsonFile(@"D:\TechLabs\genalgo_cfg.json");
+var test = new EvolutionNoGui(cfg, new ConsoleLogger());
+
+var population = test.GenerateStarterPopulation();
+var gen = 1;
+while(gen <= 100)
+    population = test.RunGeneration(gen++, population);
