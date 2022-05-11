2c5956381c5e32d35ec7a86e4a130a6981700a90
Andreev Artem <andreev_3731@mail.ru>
2022-05-11 10:52:37 +0300

    feat: add benchmarking

--- Lab4/GeneticAlgo/PerfTipsPlayground/Program.cs
+++ Lab4/GeneticAlgo/PerfTipsPlayground/Program.cs
@@ -1,13 +1,18 @@
-﻿using AlgorithmLogic.Configuration;
-using AlgorithmLogic.Evolution;
-using AlgorithmLogic.Tools.Loggers;
+﻿// using AlgorithmLogic.Configuration;
+// using AlgorithmLogic.Evolution;
+// using AlgorithmLogic.Tools.Loggers;
+//
+//
+// var reader = new JsonConfigReader();
+// var cfg = reader.ReadFromJsonFile(@"D:\TechLabs\genalgo_cfg.json");
+// var test = new EvolutionNoGui(cfg, new ConsoleLogger());
+//
+// var population = test.GenerateStarterPopulation();
+// var gen = 1;
+// while(gen <= 100)
+//     population = test.RunGeneration(gen++, population);
 
+using BenchmarkDotNet.Running;
+using PerfTipsPlayground;
 
-var reader = new JsonConfigReader();
-var cfg = reader.ReadFromJsonFile(@"D:\TechLabs\genalgo_cfg.json");
-var test = new EvolutionNoGui(cfg, new ConsoleLogger());
-
-var population = test.GenerateStarterPopulation();
-var gen = 1;
-while(gen <= 100)
-    population = test.RunGeneration(gen++, population);
+var summary = BenchmarkRunner.Run<Benchmarking>();
